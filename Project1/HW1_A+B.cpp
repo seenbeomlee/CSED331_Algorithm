d784529453b1a83e406d4cd4b04afbdc79244c63
Seenbeom Lee <mail@seenblee.com>
2020-03-23 14:36:55 +0900

    first test to commit

---
+++ Project1/HW1_A+B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/// git
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		long long a, b;
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}
