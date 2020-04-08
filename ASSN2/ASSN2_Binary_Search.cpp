93c029871c9d7b68ec32659be199e32dddd6a436
Seenbeom Lee <mail@seenblee.com>
2020-04-08 19:55:20 +0900

    ASSN2

---
+++ ASSN2/ASSN2_Binary_Search.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// list와 query 숫자 long long으로 받아야 함
+
+	int t, n, m;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		cin >> n >> m;
+	}
+
+	return 0;
+}
---
+++ ASSN2/ASSN2_Maximum_Subsequence_Sum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int t, N;
+	long long temp, current_sum, max_sum;
+	long long first_elem;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> N;
+		cin >> first_elem; // read A[0]
+		current_sum = max_sum = first_elem;
+		for (int j = 1; j < N; j++) {
+			cin >> temp;
+			current_sum += temp;
+			if (current_sum > max_sum) max_sum = current_sum;
+			else if (current_sum < 0) {
+				current_sum = 0;
+			}
+			if (temp > max_sum) {
+				max_sum = temp;
+				current_sum = temp;
+			}
+			if (temp == 0 && max_sum < 0) max_sum = 0;
+
+		}
+		cout << max_sum << endl;
+	}
+}
