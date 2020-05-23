8877853bac430bee8bb487e31522f544323763aa
Seenbeom Lee <mail@seenblee.com>
2020-05-23 14:08:42 +0900

    Robots, 2D-Convolution / Antarctic-Adventure, Antarctic-Adventure-Hard

--- ASSN5/ASSN5_2D-Convolution.cpp
+++ ASSN5/ASSN5_2D-Convolution.cpp
@@ -13,31 +13,49 @@ int main() {
 		cin >> N >> M >> d;
 
 		int** arr = new int* [N];
+		long long** DP = new long long* [N];
+
 		for (int j = 0; j < N; j++) {
 			arr[j] = new int[M];
+			DP[j] = new long long[M];
 		}
 
-		for (int j = 0; j < N; j++)
+		for(int j = 0; j < N; j++)
 			for (int k = 0; k < M; k++)
 				cin >> arr[j][k];
+		
+		DP[0][0] = arr[0][0];
 
-		long long** col_sum = new long long* [N - d + 1];
-		for (int j = 0; j < N - d + 1; j++) col_sum[j] = new long long[M];
-
-		for (int j = 0; j < N - d + 1; j++)
-			for (int k = 0; k < M; k++)
-				col_sum[j][k] = 0;
-
-		for (int j = 0; j < N - d + 1; j++)
+		for (int j = 0; j < N; j++)
 			for (int k = 0; k < M; k++) {
-				for (int q = j; q < j + d; q++) col_sum[j][k] += arr[q][k];
+				if (j == 0) { /* first row */
+					if (k == 0)continue;
+					DP[j][k] = DP[j][k - 1] + arr[j][k];
+				}
+				else if (k == 0) { /* first column */
+					DP[j][k] = DP[j - 1][k] + arr[j][k];
+				}
+				else {
+					DP[j][k] = arr[j][k] + DP[j - 1][k] + DP[j][k - 1] - DP[j - 1][k - 1];
+				}
 			}
-
+		/*
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < M; k++)
+				cout << DP[j][k] << " ";
+			cout << endl;
+		}
+		*/
 		for (int j = 0; j < N - d + 1; j++) {
-			for (int k = 0; k < M - d + 1; k++) { /* convol matrix's top left elem */
-				RESULT = 0;
-				for (int q = k; q < k + d; q++) RESULT += col_sum[j][q];
-				cout << RESULT << " ";
+			for (int k = 0; k < M - d + 1; k++) {
+				if (j == 0 && k == 0)
+					cout << DP[j + d - 1][k + d - 1] << " ";
+				else if (j > 0 && k == 0)
+					cout << DP[j + d - 1][k + d - 1] - DP[j - 1][k + d - 1] << " ";
+				else if (j == 0 && k > 0)
+					cout << DP[j + d - 1][k + d - 1] - DP[j + d - 1][k - 1] << " ";
+				else if(j - 1 >= 0 && k - 1 >= 0)
+					cout << DP[j + d - 1][k + d - 1] - DP[j + d - 1 - d][k + d - 1] - DP[j + d - 1][k + d - 1 - d] + DP[j + d - 1 - d][k + d - 1 - d] << " ";
 			}
 			cout << endl;
 		}
